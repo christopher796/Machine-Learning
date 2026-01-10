d36571fb53f2a82f017247ef7c8d51a434745d5b
christopher796 <ndaruchristopher100@gmail.com>
2026-01-10 10:56:28 +0300

    Multiple Regression

---
+++ multiple_regression3.py
@@ -0,0 +1,13 @@
+import pandas
+from sklearn import linear_model
+
+df = pandas.read_csv("C:\\Users\\Admin\\Downloads\\data.csv")
+
+X = df[['Weight', 'Volume']]
+y = df['CO2']
+
+regr = linear_model.LinearRegression()
+regr.fit(X, y)
+
+predictedCO2 = regr.predict([[2500, 1400]])
+print(predictedCO2)
