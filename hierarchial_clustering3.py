8c988fdf74354a167ef01801c39dc9e09cc8b51d
christopher796 <ndaruchristopher100@gmail.com>
2026-01-10 18:49:37 +0300

    Hierarchial clustering

---
+++ hierarchial_clustering3.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import AgglomerativeClustering
+
+X = [4,5,10,4,3,11,14,6,10,12]
+y = [21,19,24,17,16,25,24,22,21,21]
+
+data = list(zip(X, y))
+
+hierarchial_cluster = AgglomerativeClustering(n_clusters=2, linkage='ward')
+
+labels = hierarchial_cluster.fit_predict(data)
+
+plt.scatter(X, y, c=labels)
+plt.show()
